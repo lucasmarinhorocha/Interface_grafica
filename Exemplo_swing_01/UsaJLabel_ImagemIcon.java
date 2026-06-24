167cc89a855a35e5c687f2a80c565e4397888c6f
Lucas Marinho Rocha <lucasmarinhorocha10@gmail.com>
2026-06-23 21:44:05 -0300

    50%

---
+++ Exemplo_swing_01/UsaJLabel_ImagemIcon.java
@@ -0,0 +1,43 @@
+package Exemplo_swing_01;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JRootPane;
+
+import java.awt.Color;
+import java.awt.GridLayout;
+
+public class UsaJLabel_ImagemIcon extends JFrame{
+    
+    private JLabel label1,label2;
+    private ImageIcon icon = new ImageIcon();
+
+    public UsaJLabel_ImagemIcon(){
+
+        setTitle("HELLOU WORLD");
+        setSize(350,250);
+        setLocation(500,250);
+        getContentPane().setBackground(Color.CYAN);
+
+        label1 = new JLabel("Esquerda",icon,JLabel.LEFT);
+        label2 = new JLabel("Direita",icon,JLabel.RIGHT);
+      
+
+        getContentPane().setLayout(new GridLayout(2,1));
+        getContentPane().add(label1);
+        getContentPane().add(label2);
+    }
+
+    public static void main(String[] args) {
+        UsaJLabel_ImagemIcon janela = new UsaJLabel_ImagemIcon();
+        janela.setUndecorated(true);
+        janela.getRootPane().setWindowDecorationStyle(JRootPane.FRAME);
+        
+        
+        
+        janela.setVisible(true);
+    }
+
+}
+
